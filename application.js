22242697e8061fa4bec7e899ceeaed1f19627ec1
spencerhass <shass615@yahoo.com>
2015-09-15 23:15:41 -0400

    changes to files

--- application.js
+++ application.js
@@ -1,7 +1,35 @@
-$(document).ready(function() {
-	$("#item").keypress(function (e) {
-		if(e.which ==13) {
-			alert('You entered an item');
+$(document).ready(function(){
+	var trash= '<span class="trash"></span>';
+	var notification= '<span class="notification"></span>';
+
+
+
+function addItem(){
+	$('#item').keydown(function(enter){
+		if(enter.keyCode ==13) {
+			postItem();
 		}
-	});		
+	});
+}
+addItem();
+
+function postItem(){
+	var item = $('#item').val();
+	var placeIt = $('<li>' +trash + item + notification +'</li>');
+	$('.list').prepend(placeIt);
+	$('#item').val('');
+}
+
+$(document).on('click', '.trash', function() {
+	$(this).closest('li').fadeOut(350);
 });
+
+$(document).on('click', '.notification', function(){
+	$(this).toggleClass('checkmark');
+	$(this).closest('li').toggleClass('cross');
+})
+});
+
+
+
+
